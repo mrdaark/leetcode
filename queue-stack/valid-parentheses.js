cb95e6c1910740fb43e2c87c5633b98fa0d8b988
Mikhail Kuznetsov <mikhail.kuznetsov@egamings.com>
2022-06-25 19:22:05 +0300

    stack

---
+++ queue-stack/valid-parentheses.js
@@ -0,0 +1,29 @@
+/** https://leetcode.com/problems/valid-parentheses/ */
+
+var isValid = function (s) {
+    const map = new Map();
+    map.set('{', '}');
+    map.set('(', ')');
+    map.set('[', ']');
+    const queue = [];
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s.charAt(i))) {
+            queue.push(s.charAt(i));
+        } else {
+            let pop = queue.pop();
+            if (map.get(pop) !== s.charAt(i)) {
+                return false;
+            }
+        };
+    };
+    return queue.length === 0;
+};
+
+console.log(isValid('((([][)]') === false);
+console.log(isValid('()[]{}') === true);
+console.log(isValid('(()()[{}])[]{}') === true);
+console.log(isValid('(()()[{}])[]{') === false);
+console.log(isValid('(()()[{()}])[]{}') === true);
+console.log(isValid('(])') === false);
+console.log(isValid(')(') === false);
+
