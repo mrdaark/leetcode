1581e889517df6deb4d46a631b523d755e3ce382
Mikhail Kuznetsov <mikhail.kuznetsov@egamings.com>
2022-07-06 00:08:00 +0300

    best-time-to-buy-and-sell-stock-with-transaction-fee

--- greedy-problems/best-time-to-buy-and-sell-stock-with-transaction-fee.js
+++ greedy-problems/best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -1,5 +1,13 @@
 var maxProfit = function (prices, fee) {
+    let profit = 0;
+    let eff = prices[0];
 
+    for (let i = 0; i < prices.length; i++) {
+        profit = Math.max(profit, prices[i] - eff - fee);
+        eff = Math.min(eff, prices[i] - profit);
+    }
+    return profit;
 };
 
 console.log(maxProfit([1, 3, 2, 8, 4, 9], 2));
+console.log(maxProfit([1, 3, 7, 5, 10, 3], 3));
